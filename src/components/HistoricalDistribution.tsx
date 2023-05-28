d8b54c39a0bddb619c689a32fb9f758ce082299d
Pavel Demin <paveldemin@me.com>
2023-05-28 22:10:36 +0200

    feat: datazoom on historical chart

--- src/components/HistoricalDistribution.tsx
+++ src/components/HistoricalDistribution.tsx
@@ -4,6 +4,7 @@ import { DataContext } from "../containers/DataContainer";
 import { useContext, useState } from "react";
 import { getCleanData } from "../lib/getCleanData";
 import ProgramSelect from "./ProgramSelect";
+import dayjs from "dayjs";
 
 const CumulativeDistribution = ({ gridArea }: { gridArea: string }) => {
   const rawData = useContext(DataContext);
@@ -15,30 +16,41 @@ const CumulativeDistribution = ({ gridArea }: { gridArea: string }) => {
     .reverse();
 
   const options: EChartsOption = {
-    grid: { top: 8, right: 8, bottom: 24, left: 36 },
+    grid: { top: 8, right: 8, bottom: 80, left: 36 },
     xAxis: {
-      type: "category",
-      data: data.map(({ date }) => date),
+      type: "time",
     },
     yAxis: {
       type: "value",
     },
     series: [
       {
-        data: data.map(({ score }) => score),
+        data: data.map(({ date, score }) => [date, score]),
         type: "line",
         smooth: true,
       },
     ],
     tooltip: {
       trigger: "axis",
+      formatter: (val) => {
+        // @ts-expect-error
+        const date = dayjs(val[0].data[0]).format("DD MMM YY");
+        // @ts-expect-error
+        const score = val[0].data[1];
+        return `<b>${score}</b></br>${date}`;
+      },
+    },
+    dataZoom: {
+      show: true,
+      bottom: 10,
+      start: 85,
     },
   };
 
   return (
     <div className="card" style={{ gridArea }}>
       <div className="cumulative-title-container">
-        <h3>Passing score dynamics</h3>
+        <h3>History of passing score by program</h3>
         <ProgramSelect value={program} setValue={setProgram} />
       </div>
       <ReactECharts option={options} />
