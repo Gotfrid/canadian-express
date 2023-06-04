e5ecaf559440b8b08e26b4a0318971495cc01692
Pavel Demin <paveldemin@me.com>
2023-06-04 17:23:07 +0200

    reformat getCleanData + test

---
+++ src/lib/getCleanData.test.ts
@@ -0,0 +1,53 @@
+import { getCleanData } from "./getCleanData";
+
+describe("getCleanData", () => {
+  test("should return an array of objects with the correct properties", () => {
+    const data: Draw[] = [
+      {
+        drawNumber: "248",
+        drawNumberURL:
+          "<a href='/content/canadasite/en/immigration-refugees-citizenship/corporate/mandate/policies-operational-instructions-agreements/ministerial-instructions/express-entry-rounds/invitations.html?q=248'>248</a>",
+        drawDate: "2023-05-10",
+        drawDateFull: "May 10, 2023",
+        drawName: "Provincial Nominee Program",
+        drawSize: "589",
+        drawCRS: "691",
+        mitext:
+          "<a href='/content/canadasite/en/immigration-refugees-citizenship/corporate/mandate/policies-operational-instructions-agreements/ministerial-instructions/express-entry-rounds/invitations.html?q=248'>Invitations to apply for permanent residence under the Express Entry system #248</a>",
+        DrawText1:
+          "<a href='/content/canadasite/en/immigration-refugees-citizenship/corporate/mandate/policies-operational-instructions-agreements/ministerial-instructions/express-entry-rounds/invitations.html?q=248'>See full text of Ministerial Instruction</a>",
+        drawText2: "Provincial Nominee Program",
+        drawDateTime: "May 10, 2023 at 14:06:47 UTC",
+        drawCutOff: "March 08, 2023 at 16:42:17 UTC",
+        drawDistributionAsOn: "May 10, 2023",
+        dd1: "535",
+        dd2: "992",
+        dd3: "55,439",
+        dd4: "1,094",
+        dd5: "3,050",
+        dd6: "21,189",
+        dd7: "16,740",
+        dd8: "13,366",
+        dd9: "59,827",
+        dd10: "12,443",
+        dd11: "12,921",
+        dd12: "10,578",
+        dd13: "11,527",
+        dd14: "12,358",
+        dd15: "67,883",
+        dd16: "35,242",
+        dd17: "5,470",
+        dd18: "225,388",
+      },
+    ];
+    const expected = [
+      {
+        date: "2020-01-01",
+        score: "123",
+        size: "123",
+        name: "Test",
+      },
+    ];
+    expect(getCleanData(data)).toEqual(expected);
+  });
+});
